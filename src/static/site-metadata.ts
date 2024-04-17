207b6f8245fa2ca4e228a2380f65b017be4af493
Jianbo Wang <jnb.wang@gmail.com>
2024-04-17 11:22:37 +0900

    change site-metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,13 +11,13 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://wjianbo.github.io/running_page',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://wjianbo.github.io',
     },
     {
       name: 'About',
